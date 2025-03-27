ac8993d2c9ad174cb7928e5a448aa950d05b1f0a
AnnBalias <annbalias@gmail.com>
2025-03-27 13:34:42 +0200

    task-2

---
+++ src/scripts/generateProducts.js
@@ -0,0 +1,18 @@
+import { PATH_DB } from '../constants/products.js';
+import { createFakeProduct } from '../utils/createFakeProduct.js';
+import fs from 'node:fs/promises';
+
+const generateProducts = async (number) => {
+  const readProducts = await fs.readFile(PATH_DB, 'utf-8');
+  const productsList = JSON.parse(readProducts);
+
+  const newProduct = Array(number).fill(0).map(createFakeProduct);
+
+  await fs.writeFile(
+    PATH_DB,
+    JSON.stringify([...productsList, ...newProduct], null, 2),
+    'utf-8',
+  );
+};
+
+generateProducts(3);
